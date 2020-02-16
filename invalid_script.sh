9709bb51bcf62320685d95ee1c8e63af55ce7342
David Valaczaki <dmv18@ic.ac.uk>
2020-02-16 19:50:03 +0000

    adds tests for lexing, includes positive integer overflow detection at lexing stage (TODO: negative overflow)

---
+++ invalid_script.sh
@@ -0,0 +1,61 @@
+for file in ../ewacc_examples/invalid/semanticErr/exit/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/expressions/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/function/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/if/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/IO/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/multiple/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/pairs/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/print/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/read/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/scope/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/variables/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/invalid/semanticErr/while/*
+do
+	echo "$file"
+	./compile "$file"
+done
+
---
+++ valid_script.sh
@@ -0,0 +1,65 @@
+for file in ../ewacc_examples/valid/advanced/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/array/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/basic/*/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/expressions/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/function/*/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/if/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/IO/*/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/pairs/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/runtimeErr/*/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/scope/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/sequence/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/while/*
+do
+	echo "$file"
+	./compile "$file"
+done
+for file in ../ewacc_examples/valid/variables/*
+do
+	echo "$file"
+	./compile "$file"
+done
